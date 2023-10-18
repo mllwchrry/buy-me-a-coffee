84bac89a2e45658d6fdad484555f81cce7f20c1d
Mllw Chrry <zhvmr09@gmail.com>
2023-10-18 20:17:32 +0300

    Update deploy.js

--- BuyMeACoffee-contracts/scripts/deploy.js
+++ BuyMeACoffee-contracts/scripts/deploy.js
@@ -9,8 +9,6 @@ async function main() {
   console.log("BuyMeACoffee deployed to:", await buyMeACoffee.getAddress());
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
